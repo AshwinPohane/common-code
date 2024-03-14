d4ffe2844bc653f1e3686cdc4b58d5eb2026b186
Ashwin Pohane <89726071+AshwinPohane@users.noreply.github.com>
2024-03-14 12:14:27 +0530

    Add files via upload

--- Jupyter/pkg_ver_code.py
+++ Jupyter/pkg_ver_code.py
@@ -1,9 +1,10 @@
-from importlib.metadata import distributions
+def requirement_pkg(name=''):
 
-installed = distributions()
+    from importlib.metadata import distributions
+    installed = distributions()
 
-with open('req.txt', 'w') as f:
-    for i in installed:
-        pkg_name = i.metadata['Name']
-        pkg_ver = i.version
-        f.write(f"{pkg_name}=={pkg_ver}\n")
+    with open(f'req{name}.txt', 'w') as f:
+        for i in installed:
+            pkg_name = i.metadata['Name']
+            pkg_ver = i.version
+            f.write(f"{pkg_name}=={pkg_ver}\n")
