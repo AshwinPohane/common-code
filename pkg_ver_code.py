6f58d17ada0dbf67b58489f23e99c7c25f74ad60
Ashwin Pohane <89726071+AshwinPohane@users.noreply.github.com>
2024-03-14 12:13:58 +0530

    Delete pkg_ver_code.py

--- pkg_ver_code.py
+++
@@ -1,9 +0,0 @@
-from importlib.metadata import distributions
-
-installed = distributions()
-
-with open('req.txt', 'w') as f:
-    for i in installed:
-        pkg_name = i.metadata['Name']
-        pkg_ver = i.version
-        f.write(f"{pkg_name}=={pkg_ver}\n")
